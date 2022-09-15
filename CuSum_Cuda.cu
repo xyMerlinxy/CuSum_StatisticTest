7c8980737b6de333f9202a006306999c7355baa9
Paweł <xymerlinxy@gmail.com>
2022-09-15 14:25:44 +0200

    Zmiana nazwy zmiennych, dodanie opisów funkcji

--- CuSum_Cuda.cu
+++ CuSum_Cuda.cu
@@ -3,25 +3,22 @@
 __device__ double cuda_SQRT_2 = 1.414213562373095048801688724209698078569672;
 
 __device__ double cuda_rozkl_norm_upr(double x){
-    if(x>0)
-        return erf(x/cuda_SQRT_2);
-    else
-        return -erf(-x/cuda_SQRT_2);
+    return (x>0)? erf(x/cuda_SQRT_2) : -erf(-x/cuda_SQRT_2);
 }
 
-__global__ void cuda_CuSum(uint8_t **arrays, size_t *tab_numElements, double * P_values, int num_datas){
+__global__ void cuda_CuSum(uint8_t **dataArray, int numBlock, size_t *blockSizeArray, double * P_values){
     int id = blockDim.x * blockIdx.x + threadIdx.x;
-    if(id < num_datas){
+    if(id < numBlock){
         double *P_value = &(P_values[id]);
-        uint8_t * array = arrays[id];
-        uint64_t numElements = tab_numElements[id];
+        uint8_t * data = dataArray[id];
+        uint64_t blockSize = blockSizeArray[id];
 
         int64_t S=0, _max=0, _min=0;
         uint8_t temp;
         // przejście po wszystkich bajtach
-        for(uint64_t index = 0; index<numElements;index++){
+        for(uint64_t index = 0; index<blockSize;index++){
             // przejscie po bitach
-            temp = array[index];
+            temp = data[index];
             for(int i=0; i<8; i++){
                 if(temp & 0x80) {
                     S++;
@@ -35,13 +32,11 @@ __global__ void cuda_CuSum(uint8_t **arrays, size_t *tab_numElements, double * P
             }
         }
         int64_t z = (_max > -_min) ? _max : -_min;
-        int64_t n = numElements*8;
+        int64_t n = blockSize*8;
 
         double sqrtNz = z / sqrt(n);
         double sum=0;
 
-        /////////////////////////////////////////////
-        //uproszczone pętle sum + upr rozkład normalny
         for(int64_t k = (-n/z-3)/4*4; k<-n/z+1; k+=4){
             sum-=cuda_rozkl_norm_upr((k+3) * sqrtNz);
             sum+=cuda_rozkl_norm_upr((k+1) * sqrtNz);
@@ -51,24 +46,22 @@ __global__ void cuda_CuSum(uint8_t **arrays, size_t *tab_numElements, double * P
             sum -= cuda_rozkl_norm_upr((k-1) * sqrtNz);
             sum -= cuda_rozkl_norm_upr((k+3) * sqrtNz);
         }
-
         (*P_value) = 1-sum/2;
     }
 }
 
-// To dziala na tablicy dancyh i dzieli go na bloki o rozmiarze numElements
-__global__ void cuda_CuSum_v2(uint8_t *arrays, size_t numElements, double * P_values, int num_datas){
+__global__ void cuda_CuSum(uint8_t *ptrData,  int numBlock, size_t blockSize, double * P_values){
     int id = blockDim.x * blockIdx.x + threadIdx.x;
-    if(id < num_datas){
+    if(id < numBlock){
         double * P_value = &(P_values[id]);
-        uint8_t * array = &arrays[numElements*id];
+        uint8_t * data = &ptrData[blockSize*id];
 
         int64_t S=0, _max=0, _min=0;
         uint8_t temp;
         // przejście po wszystkich bajtach
-        for(uint64_t index = 0; index<numElements;index++){
+        for(uint64_t index = 0; index<blockSize;index++){
             // przejscie po bitach
-            temp = array[index];
+            temp = data[index];
             for(int i=0; i<8; i++){
                 if(temp & 0x80) {
                     S++;
@@ -82,13 +75,11 @@ __global__ void cuda_CuSum_v2(uint8_t *arrays, size_t numElements, double * P_va
             }
         }
         int64_t z = (_max > -_min) ? _max : -_min;
-        int64_t n = numElements*8;
+        int64_t n = blockSize*8;
 
         double sqrtNz = z / sqrt(n);
         double sum=0;
 
-        /////////////////////////////////////////////
-        //uproszczone pętle sum + upr rozkład normalny
         for(int64_t k = (-n/z-3)/4*4; k<-n/z+1; k+=4){
             sum-=cuda_rozkl_norm_upr((k+3) * sqrtNz);
             sum+=cuda_rozkl_norm_upr((k+1) * sqrtNz);
--- CuSum_Cuda.hpp
+++ CuSum_Cuda.hpp
@@ -3,7 +3,17 @@
 
 #include <iostream>
 
-__global__ void cuda_CuSum(uint8_t **arrays, size_t *tab_numElements, double * P_values, int num_datas);
-__global__ void cuda_CuSum_v2(uint8_t *arrays, size_t numElements, double * P_values, int num_datas);
+// Funkcja realizuje test CuSum blokach danych o różnej długości.
+// dataArray - tablica zawierająca tablice z danymi
+// numBlock - ilość bloków danych
+// blockSizeArray - tablica rozmiarów bloku
+// Wyznaczana wartość P-value zapisywana jest do tablicy P_values.
+__global__ void cuda_CuSum(uint8_t **dataArray, int numBlock, size_t *blockSizeArray, double * P_values);
+// Funkcja realizuje test CuSum blokach danych o tej samej długości.
+// ptrData - wskaźnik na dane
+// numBlock - ilość bloków danych
+// blockSize - rozmiar bloku
+// Wyznaczana wartość P-value zapisywana jest do tablicy P_values.
+__global__ void cuda_CuSum(uint8_t *ptrData,  int numBlock, size_t blockSize, double * P_values);
 
 #endif

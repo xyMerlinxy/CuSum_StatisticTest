3bd0bc87458b85a3adeda12ce39b7a050c2a8eca
Paweł <xymerlinxy@gmail.com>
2022-09-28 11:22:51 +0200

    Cpp file

---
+++ CuSum.cpp
@@ -0,0 +1,48 @@
+#include <math.h>
+
+#include "CuSum.hpp"
+
+double SQRT_2 = 1.414213562373095048801688724209698078569672;
+
+// ta funkcja implementuje uproszczona funkjcę noramlną
+// w celu zwiększenia szybkości wykonania niektore jej elementy zostały przesunięte do funkcji nadrzędnej
+double rozkl_norm_upr(double x){
+    if(x>0)
+        return erf(x/SQRT_2);
+    else
+        return -erf(-x/SQRT_2);
+}
+
+double CuSum(uint8_t *array, uint64_t numElements){
+    int64_t S=0, _max=0, _min=0;
+    uint8_t temp;
+    // przejście po wszystkich bajtach
+    for(uint64_t index = 0; index<numElements;index++){
+        // przejscie po bitach
+        temp = array[index];
+        for(int i=0; i<8; i++){
+            if(temp & 0x80) S++;
+            else S--;
+            temp <<= 1;
+            if(S < _min) _min--;
+            if(S > _max) _max++;
+        }
+    }
+    int64_t z = (_max > -_min) ? _max : -_min;
+    int64_t n = numElements*8;
+
+    double sqrtNz = z / sqrt(n);
+    double sum=0;
+
+    for(int64_t k = (-n/z-3)/4*4; k<-n/z+1; k+=4){
+        sum-=rozkl_norm_upr((k+3) * sqrtNz);
+        sum+=rozkl_norm_upr((k+1) * sqrtNz);
+    }
+    for(int64_t k = (-n/z+1)/4*4; k<=n/z-1; k+=4){
+        sum += rozkl_norm_upr((k+1) * sqrtNz) * 2;
+        sum -= rozkl_norm_upr((k-1) * sqrtNz);
+        sum -= rozkl_norm_upr((k+3) * sqrtNz);
+    }
+    double P_value = 1-sum/2;
+    return P_value;
+}
---
+++ CuSum.hpp
@@ -0,0 +1,8 @@
+#ifndef CUSUM_HPP
+#define CUSUM_HPP
+
+#include <iostream>
+
+double CuSum(uint8_t *array, uint64_t numElements);
+
+#endif

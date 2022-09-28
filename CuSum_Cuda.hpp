2e9f69b3c24fa44ec6b5d405a1357b5ad51a75dd
Paweł Kluczewski <76821282+xyMerlinxy@users.noreply.github.com>
2022-09-28 14:38:55 +0200

    Update CuSum_Cuda.hpp
    Delete cuda_CuSum_v2

--- CuSum_Cuda.hpp
+++ CuSum_Cuda.hpp
@@ -16,12 +16,4 @@ __global__ void cuda_CuSum(uint8_t **dataArray, int numBlock, size_t *blockSizeA
 // Wyznaczana wartość P-value zapisywana jest do tablicy P_values.
 __global__ void cuda_CuSum(uint8_t *ptrData,  int numBlock, size_t blockSize, double * P_values);
 
-
-
-// Funkcja realizuje test CuSum blokach danych o tej samej długości.
-// ptrData - wskaźnik na dane
-// numBlock - ilość bloków danych
-// blockSize - rozmiar bloku
-// Wyznaczana wartość P-value zapisywana jest do tablicy P_values.
-__global__ void cuda_CuSum_v2(uint8_t *ptrData,  int numBlock, size_t blockSize, double * P_values);
 #endif
